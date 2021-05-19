c4b1c88bb994285cba04dcdf827b1d22155a55b5
Joel Rdz <joelrdzh@gmail.com>
2021-05-19 13:07:17 -0500

    08-adding-routing

---
+++ 08-adding-routing/src/App.js
@@ -0,0 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+
+function App() {
+  return (
+    <div>
+      <Route path='/' exact>
+        <AllMeetupsPage />
+      </Route>
+      <Route path='/new-meetup'>
+        <NewMeetupPage />
+      </Route>
+      <Route path='/favorites'>
+        <FavoritesPage />
+      </Route>
+    </div>
+  );
+}
+
+export default App;
