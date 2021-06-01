fa0cc068e63f1f863d577981653351f3e1b6a53d
Joel Rdz <joelrdzh@gmail.com>
2021-06-01 12:02:42 -0500

    16-sending-a-post-request

---
+++ 16-sending-a-post-request/src/pages/NewMeetup.js
@@ -0,0 +1,22 @@
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
+function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch('https://academind-react-c31e5-default-rtdb.firebaseio.com/meetups.json', {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
