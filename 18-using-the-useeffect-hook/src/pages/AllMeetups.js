dce821feb13faf26265a7a325764984bb1979b6d
Joel Rdz <joelrdzh@gmail.com>
2021-06-01 18:27:32 -0500

    18-using-the-useeffect-hook

---
+++ 18-using-the-useeffect-hook/src/pages/AllMeetups.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+
+function AllMeetupsPage() {
+  const [loading, setLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://academind-react-c31e5-default-rtdb.firebaseio.com/meetups.json')
+      .then(response => {
+        return response.json();
+      }).then(data => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key]
+          };
+
+          meetups.push(meetup);
+        }
+
+        setLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (loading) {
+    return(
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
+}
+
+export default AllMeetupsPage;
